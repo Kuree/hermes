e1b96f467852a5f46f1f97dcfa8308eb1e8e403e
Keyi Zhang <keyi@stanford.edu>
2021-04-07 13:25:32 -0700

    add python binding for query helper

--- src/python/_pyhermes.cc
+++ src/python/_pyhermes.cc
@@ -4,6 +4,7 @@
 
 #include "../loader.hh"
 #include "../logger.hh"
+#include "../query.hh"
 #include "../serializer.hh"
 #include "../tracker.hh"
 
@@ -180,6 +181,36 @@ void init_tracker(py::module &m) {
                          &hermes::Tracker::set_transaction_name);
 }
 
+void init_query(py::module &m) {
+    auto helper = py::class_<hermes::QueryHelper>(m, "QueryHelper");
+    helper.def(py::init<std::shared_ptr<hermes::Loader>>());
+    helper.def("concurrent_events",
+               py::overload_cast<uint64_t>(&hermes::QueryHelper::concurrent_events));
+    helper.def("concurrent_events",
+               py::overload_cast<uint64_t, uint64_t>(&hermes::QueryHelper::concurrent_events));
+    helper.def("concurrent_events", py::overload_cast<const std::string &, uint64_t, uint64_t>(
+                                        &hermes::QueryHelper::concurrent_events));
+    helper.def("concurrent_events", py::overload_cast<const std::shared_ptr<hermes::Event> &>(
+                                        &hermes::QueryHelper::concurrent_events));
+    helper.def("concurrent_events",
+               py::overload_cast<const std::string &, const std::shared_ptr<hermes::Event> &>(
+                   &hermes::QueryHelper::concurrent_events));
+
+    helper.def("concurrent_transactions",
+               py::overload_cast<uint64_t>(&hermes::QueryHelper::concurrent_transactions));
+    helper.def("concurrent_transactions", py::overload_cast<uint64_t, uint64_t>(
+                                              &hermes::QueryHelper::concurrent_transactions));
+    helper.def("concurrent_transactions",
+               py::overload_cast<const std::string &, uint64_t, uint64_t>(
+                   &hermes::QueryHelper::concurrent_transactions));
+    helper.def("concurrent_transactions",
+               py::overload_cast<const std::shared_ptr<hermes::Transaction> &>(
+                   &hermes::QueryHelper::concurrent_transactions));
+    helper.def("concurrent_transactions",
+               py::overload_cast<const std::string &, const std::shared_ptr<hermes::Transaction> &>(
+                   &hermes::QueryHelper::concurrent_transactions));
+}
+
 void init_message_bus(py::module &m) {
     auto bus = py::class_<hermes::MessageBus, std::shared_ptr<hermes::MessageBus>>(m, "MessageBus");
     bus.def("flush", &hermes::MessageBus::stop);
@@ -200,4 +231,5 @@ PYBIND11_MODULE(_pyhermes, m) {
     init_tracker(m);
     init_message_bus(m);
     init_loader(m);
+    init_query(m);
 }
