753cc37d34cc8fb0969d5baf431d7919cc6b7795
Keyi Zhang <keyi@stanford.edu>
2021-04-10 17:48:29 -0700

    add checker implementation; need to add protection in the loader (for cache)

--- src/checker.cc
+++ src/checker.cc
@@ -1,12 +1,42 @@
 #include "checker.hh"
 
+#include <thread>
+
 namespace hermes {
 
 void Checker::run(const std::string &transaction_name, const std::shared_ptr<Loader> &loader) {
-    if (transaction_name.empty()) {
-        // load all transactions
-        
+    auto tables =
+        loader->load_transaction_table(transaction_name, 0, std::numeric_limits<uint64_t>::max());
+    // depends on whether it is stateless or not
+    if (stateless_) {
+        // run it in parallel. each thread gets a table
+        std::vector<std::thread> threads;
+        threads.reserve(tables.size());
+        for (auto const &table : tables) {
+            std::vector<std::shared_ptr<arrow::Table>> ts = {table.table};
+            auto thread = std::thread([ts, loader, this]() {
+                auto stream = TransactionStream(ts, loader.get());
+                for (auto &&it : stream) {
+                    check(it.transaction);
+                }
+            });
+            threads.emplace_back(std::move(thread));
+        }
+        for (auto &thread : threads) {
+            thread.join();
+        }
+    } else {
+        // linear
+        std::vector<std::shared_ptr<arrow::Table>> ts;
+        ts.reserve(tables.size());
+        for (auto const &res : tables) {
+            ts.emplace_back(res.table);
+        }
+        auto stream = TransactionStream(ts, loader.get());
+        for (auto &&it : stream) {
+            check(it.transaction);
+        }
     }
 }
 
-}
+}  // namespace hermes
