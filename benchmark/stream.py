da9fac188211b8ee29ca77d236907c964ab545b2
Keyi Zhang <keyi@stanford.edu>
2021-04-25 12:20:57 -0700

    add in memory read as well

--- benchmark/stream.py
+++ benchmark/stream.py
@@ -75,10 +75,21 @@ def main():
         assert e.id >= 0
     end = time.time()
     reading_speed = end - start
+    start = time.time()
+    stream = loader["test"]
+    count = 0
+    for e in stream:
+        count += 1
+        assert e.id >= 0
+    end = time.time()
+    in_memory_speed = end - start
+
     num_transaction = args.num_events / args.transaction_size
     assert count == num_transaction
     print("Write:", "{0:.2f} ms/t".format(writing_speed / num_transaction * 1000), "Total:", f"{writing_speed:.2f}s")
     print("Read:", "{0:.2f} ms/t".format(reading_speed / num_transaction * 1000), "Total:", f"{reading_speed:.2f}s")
+    print("In-Memory Read:", "{0:.2f} ms/t".format(in_memory_speed / num_transaction * 1000), "Total:",
+          f"{in_memory_speed:.2f}s")
     print("Loading:", f"{meta_loading:.2f}s")
 
     # clean up
